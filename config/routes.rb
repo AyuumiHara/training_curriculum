1e89c1edbfe9f7b3e5c731bb488f1c9615236071
AyuumiHara <ayuumi027289@gmail.com>
2021-02-26 11:45:28 +0900

    Issue4

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'calendars#index'
-  resources :calendars
+  get 'plans', to: 'calendars#create'
+  resources :calendars, only: [:index, :new, :create]
 end
